7a495f012f13f38e9fd784af323c42f4797923ed
YE HTUT WIN <mimie.yhw18@gmail.com>
2026-03-05 11:12:02 +0700

    0014-LONGEST-COMMON-PREFIX Stats: Time: 0 ms (100%), Space: 12.5 MB (64.33%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,21 +4,6 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        cm_prefix = ""; condition = True
-        if len(strs) > 1:
-            for i in range(len(strs)):
-                if i == 0:
-                    cm_prefix = strs[i]
-                else:
-                    for j in range(len(cm_prefix)):
-                        if j < len(strs[i]) and cm_prefix[j] == strs[i][j]:
-                            continue
-                        else:
-                            cm_prefix = cm_prefix[:j]
-                            break   
-        else:
-            return strs[0]
-        return cm_prefix 
-                
+        return os.path.commonprefix(strs)
                     
         
