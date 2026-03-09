3a06f215654976bced18e3269dc5bc30abe92b26
YE HTUT WIN <mimie.yhw18@gmail.com>
2026-03-09 14:08:23 +0700

    0088-MERGE-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 12.4 MB (53.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,35 +7,10 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        if m==0:
-            for k in range(len(nums2)):
-                nums1[k]=nums2[k]
-            return nums1
-        elif n==0:
-            return nums1
-        else:
-            i=0;j=0;dummy=[]
-            while True:
-                if i==m:
-                    dummy+=nums2[j:]
-                    print(dummy)
-                    for m in range(len(dummy)):
-                        nums1[m]=dummy[m]
-                    return nums1
-                elif j==n:
-                    len_num1 = len(nums1)-m
-                    dummy += nums1[i:]
-                    for n in range(len_num1):
-                        dummy.pop(-1)
-                    for o in range(len(dummy)):
-                        nums1[o]=dummy[o]
-                    return nums1
-                elif nums1[i]<=nums2[j]:
-                    dummy.append(nums1[i])
-                    i+=1
-                else:
-                    dummy.append(nums2[j])
-                    j+=1
+        for i in range(n):
+            nums1[m+i]=nums2[i]
+        nums1.sort()
+        return nums1
 
             
 
